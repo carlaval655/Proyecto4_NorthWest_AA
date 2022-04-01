a540dbca54a42cd8ba5a36f0af52e53f8c50950b
carlaval655 <carla.val655@gmail.com>
2022-04-01 02:25:42 -0400

    Registro ofertas y demandas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var baseDatos = [];
-
+var demandas = [];
+var ofertas = [];
 function valorActual(buscar) {
     let i = 0;
     baseDatos.forEach(function (elemento) {
@@ -186,361 +187,41 @@ window.onload = function contactanos(){
     $('#informacionContactanos').html(html);
 }
 
-//Creamos la matriz de adyacencia 
-function matrizMinimizacion(){
-    var elementos = document.getElementsByClassName("control");
-    var vertices = [];
-    let dimension = 0;
-    //Se calcula la dimension de la matriz de acuerdo a la cantidad de vertices que hay en el grafo
-    for (i = 1; i < elementos.length; i++) {
-        vertices.push(elementos[i].id);
-        dimension++;
+function mostrarInputOfertasyDemandas(){
+    var origen = document.getElementsByClassName('origen');
+    let htmlOrigen=`Ingrese los siguientes valores de las ofertas: <br><center>`;
+    for (i=1; i<origen.length; i++){
+        htmlOrigen+=`<label>
+        Oferta - Origen ${i}   </label><input onKeypress="if (event.keyCode < 45 || event.keyCode > 57) event.returnValue = false;"
+        style="height:2vw; margin-left:1.5vw;" id="of-${i}"><br>`;
     }
-    var matriz = [];
-    var a1 = [];
-    var a2 = [];
-    //Creamos y paralelamente cereamos la matriz
-    for (var f=0; f<dimension; f++){
-        //Creamos un nuevo array que representa cada fila dentro de la tabla
-        var nuevoArray=[];
-        for (var c=0; c<dimension; c++){
-            //Hacemos push de ceros en el array de acuerdo a la cantidad de columnas que deseamos que tenga
-            nuevoArray.push(0);
-        }
-        // Por ultimo hacemos un push del array dentro de la matriz habiendo concluido una fila
-        matriz.push(nuevoArray);
-        a1.push(nuevoArray);
-        a2.push(nuevoArray);
+    htmlOrigen+=`</center>`;
+    $('#ofertas').html(htmlOrigen);
+    var destino = document.getElementsByClassName('destino');
+    let htmlDestino=`Ingrese los siguientes valores de las demandas: <br><center>`;
+    for (i=1; i<destino.length; i++){
+        htmlDestino+=`<label>
+        Demanda - Destino ${i}   </label><input onKeypress="if (event.keyCode < 45 || event.keyCode > 57) event.returnValue = false;"
+        style="height:2vw; margin-left:1.5vw;" id="de-${i}"><br>`;
     }
-    //Llenamos la matriz con los atributos de la BDD
-    for (f = 0; f < baseDatos.length; f++) {
-        var origen = baseDatos[f]["llave"].substr(0, 36);
-        var destino = baseDatos[f]["llave"].substr(-36);
-        var posicionFila = parseInt(vertices.indexOf(origen),10);
-        var posicionColumna = parseInt(vertices.indexOf(destino),10);
-        matriz[posicionFila][posicionColumna] = parseInt(baseDatos[f].valor,10);
-    }
-    var minimosColumnas = [];
-    for (c=0; c<dimension; c++){
-        minimosColumnas.push(matriz[0][c]);
-    }
-    for (f=0; f<dimension; f++){
-        for (c=0; c<dimension; c++){
-            if (matriz[f][c]<minimosColumnas[c]){
-                minimosColumnas[c]=matriz[f][c];
-            }
-        }
-    }
-    let html = `
-        <table>
-            <tr>
-                <th></th>`;
-    for (c = 0; c < verticesColumnas; c++) {
-        html += `<th>${getNombreEstacion(c)}</th>`;
-    }
-    html += `</tr>`;
-
-    for (f = 0; f <d; f++) {
-        html += `<tr><td>${getNombreEstacion(f)}</td>`;
-        for (c = 0; c < d; c++) {
-            html += `<td>${matriz[f][c]}</td>`;
-        }
-        html += `</tr>`;
-    }
-
-    html+= `<tr>`;
-    html+= `<td>Minimos</td>`
-    for (c=0; c<d; c++){
-        html+=`<td>${minCol[c]}</td>`
-    }
-    html+=`</tr>`;
-    html += `</table>`;
-    $(ubicacion).html(html);
+    htmlDestino+=`</center`;
+    $('#demandas').html(htmlDestino);
 }
-function generarMatrizA1Min(matriz,minCol, d){
-    console.log("Matriz Inicial: ", matriz);
-    console.log("minCol: ", minCol);
-    var a1 = [];
-    for (var f=0; f<d; f++){
-        var nuevoArray=[];
-        for (var c=0; c<d; c++){
-            nuevoArray.push(0);
-        }
-        a1.push(nuevoArray);
+function guardarOfDem(){
+    ofertas = [];
+    demandas = [];
+    var origen = document.getElementsByClassName('origen');
+    for (i=1; i<origen.length; i++){
+        var of = document.getElementById('of-'+i).value;
+        var dato = {id: origen[i].id, oferta: of};
+        ofertas.push(dato);
     }
-    for(f=0; f<d; f++){
-        for (c=0;c<d; c++){
-            a1[f][c]=parseInt(matriz[f][c],10)-parseInt(minCol[c],10);
-        }
+    var destino = document.getElementsByClassName('destino');
+    for (i=1; i<destino.length; i++){
+        var dem = document.getElementById('de-'+i).value;
+        var dato = {id: destino[i].id, demanda: dem};
+        demandas.push(dato);
     }
-
-    
-    var minimosFilas = [];
-    for (c=0; c<d; c++){
-        minimosFilas.push(a1[c][0]);
-    }
-    for (f=0; f<d; f++){
-        for (c=0; c<d; c++){
-            if (a1[f][c]<minimosFilas[f]){
-                minimosFilas[f]=a1[f][c];
-            }
-        }
-    }
-    mostrarTablaMatrizA1Min(a1, minimosFilas, '#a1-minimizacion', d);
-    generarMatrizA2Min(a1,minimosFilas,d);
-}
-function generarMatrizA2Min(a1,minFil, d){
-    console.log("Matriz A1: ", a1);
-    console.log("Min Fil: ", minFil);
-    var a2 = [];
-    for (var f=0; f<d; f++){
-        var nuevoArray=[];
-        for (var c=0; c<d; c++){
-            nuevoArray.push(0);
-        }
-        a2.push(nuevoArray);
-    }
-    console.log("Matriz a2: ", a2);
-    for(f=0; f<d; f++){
-        for (c=0;c<d; c++){
-            a2[f][c]=parseInt(a1[f][c],10)-parseInt(minFil[f],10);
-        }
-    }
-    mostrarTablaMatrizA2Min(a2,'#a2-minimizacion', d);
-    console.log("Matriz a2: ", a2);
-}
-function mostrarTablaMatrizMin(matriz, minCol, ubicacion,d){
-    let html = `
-        <table>
-            <tr>
-                <th></th>`;
-    for (c = 0; c < d; c++) {
-        html += `<th>${getNombreEstacion(c)}</th>`;
-    }
-    html += `</tr>`;
-
-    for (f = 0; f <d; f++) {
-        html += `<tr><td>${getNombreEstacion(f)}</td>`;
-        for (c = 0; c < d; c++) {
-            html += `<td>${matriz[f][c]}</td>`;
-        }
-        html += `</tr>`;
-    }
-
-    html+= `<tr>`;
-    html+= `<td>Minimos</td>`
-    for (c=0; c<d; c++){
-        html+=`<td>${minCol[c]}</td>`
-    }
-    html+=`</tr>`;
-    html += `</table>`;
-    $(ubicacion).html(html);
-}
-function mostrarTablaMatrizA1Min(matriz, minFil, ubicacion,d){
-    let html = `
-        <table>
-            <tr>
-                <th></th>`;
-    for (c = 0; c < d; c++) {
-        html += `<th>${getNombreEstacion(c)}</th>`;
-    }
-    html+=`<th>Minimos</th>`
-    html += `</tr>`;
-
-    for (f = 0; f < d; f++) {
-        html += `<tr><td>${getNombreEstacion(f)}</td>`;
-        for (c = 0; c < d; c++) {
-            html += `<td>${matriz[f][c]}</td>`;
-        }
-        html+=`<td>${minFil[f]}</td>`
-        html += `</tr>`;
-    }
-    html += `</table>`;
-    $(ubicacion).html(html);
-}
-function mostrarTablaMatrizA2Min(matriz, ubicacion, d){
-    let html = `
-        <table>
-            <tr>
-                <th></th>`;
-    for (c = 0; c < d; c++) {
-        html += `<th>${getNombreEstacion(c)}</th>`;
-    }
-    html += `</tr>`;
-
-    for (f = 0; f < d; f++) {
-        html += `<tr><td>${getNombreEstacion(f)}</td>`;
-        for (c = 0; c < d; c++) {
-            html += `<td>${matriz[f][c]}</td>`;
-        }
-        html += `</tr>`;
-    }
-    html += `</table>`;
-    $(ubicacion).html(html);
-}
-
-
-function matrizMaximizacion(){
-    var elementos = document.getElementsByClassName("control");
-    var vertices = [];
-    let dimension = 0;
-    //Se calcula la dimension de la matriz de acuerdo a la cantidad de vertices que hay en el grafo
-    for (i = 1; i < elementos.length; i++) {
-        vertices.push(elementos[i].id);
-        dimension++;
-    }
-    var matriz = [];
-    //Creamos y paralelamente cereamos la matriz
-    for (var f=0; f<dimension; f++){
-        //Creamos un nuevo array que representa cada fila dentro de la tabla
-        var nuevoArray=[];
-        for (var c=0; c<dimension; c++){
-            //Hacemos push de ceros en el array de acuerdo a la cantidad de columnas que deseamos que tenga
-            nuevoArray.push(0);
-        }
-        // Por ultimo hacemos un push del array dentro de la matriz habiendo concluido una fila
-        matriz.push(nuevoArray);
-    }
-    //Llenamos la matriz con los atributos de la BDD
-    for (f = 0; f < baseDatos.length; f++) {
-        var origen = baseDatos[f]["llave"].substr(0, 36);
-        var destino = baseDatos[f]["llave"].substr(-36);
-        var posicionFila = parseInt(vertices.indexOf(origen),10);
-        var posicionColumna = parseInt(vertices.indexOf(destino),10);
-        matriz[posicionFila][posicionColumna] = parseInt(baseDatos[f].valor,10);
-    }
-    var maximosColumnas = [];
-    for (c=0; c<dimension; c++){
-        maximosColumnas.push(matriz[0][c]);
-    }
-    for (f=0; f<dimension; f++){
-        for (c=0; c<dimension; c++){
-            if (matriz[f][c]>maximosColumnas[c]){
-                maximosColumnas[c]=matriz[f][c];
-            }
-        }
-    }
-    mostrarTablaMatrizMax(matriz,maximosColumnas, '#matriz-maximizacion', dimension);
-    generarMatrizA1Max(matriz,maximosColumnas, dimension);
-}
-function generarMatrizA1Max(matriz,maxCol, d){
-    console.log("Matriz Inicial: ", matriz);
-    console.log("maxCol: ", maxCol);
-    var a1 = [];
-    for (var f=0; f<d; f++){
-        var nuevoArray=[];
-        for (var c=0; c<d; c++){
-            nuevoArray.push(0);
-        }
-        a1.push(nuevoArray);
-    }
-    for(f=0; f<d; f++){
-        for (c=0;c<d; c++){
-            a1[f][c]=parseInt(matriz[f][c],10)-parseInt(maxCol[c],10);
-        }
-    }
-
-    
-    var maximosFilas = [];
-    for (c=0; c<d; c++){
-        maximosFilas.push(a1[c][0]);
-    }
-    for (f=0; f<d; f++){
-        for (c=0; c<d; c++){
-            if (a1[f][c]>maximosFilas[f]){
-                maximosFilas[f]=a1[f][c];
-            }
-        }
-    }
-    mostrarTablaMatrizA1Max(a1, maximosFilas, '#a1-maximizacion', d);
-    generarMatrizA2Max(a1,maximosFilas,d);
-}
-function generarMatrizA2Max(a1,maxFil, d){
-    console.log("Matriz A1: ", a1);
-    console.log("MaxFil: ", maxFil);
-    var a2 = [];
-    for (var f=0; f<d; f++){
-        var nuevoArray=[];
-        for (var c=0; c<d; c++){
-            nuevoArray.push(0);
-        }
-        a2.push(nuevoArray);
-    }
-    console.log("Matriz a2: ", a2);
-    for(f=0; f<d; f++){
-        for (c=0;c<d; c++){
-            a2[f][c]=parseInt(a1[f][c],10)-parseInt(maxFil[f],10);
-        }
-    }
-    mostrarTablaMatrizA2Max(a2,'#a2-maximizacion', d);
-    console.log("Matriz a2: ", a2);
-}
-function mostrarTablaMatrizMax(matriz, maxCol, ubicacion,d){
-    let html = `
-        <table>
-            <tr>
-                <th></th>`;
-    for (c = 0; c < d; c++) {
-        html += `<th>${getNombreEstacion(c)}</th>`;
-    }
-    html += `</tr>`;
-
-    for (f = 0; f <d; f++) {
-        html += `<tr><td>${getNombreEstacion(f)}</td>`;
-        for (c = 0; c < d; c++) {
-            html += `<td>${matriz[f][c]}</td>`;
-        }
-        html += `</tr>`;
-    }
-
-    html+= `<tr>`;
-    html+= `<td>Maximos</td>`
-    for (c=0; c<d; c++){
-        html+=`<td>${maxCol[c]}</td>`
-    }
-    html+=`</tr>`;
-    html += `</table>`;
-    $(ubicacion).html(html);
-}
-function mostrarTablaMatrizA1Max(matriz, maxFil, ubicacion,d){
-    let html = `
-        <table>
-            <tr>
-                <th></th>`;
-    for (c = 0; c < d; c++) {
-        html += `<th>${getNombreEstacion(c)}</th>`;
-    }
-    html+=`<th>Maximos</th>`
-    html += `</tr>`;
-
-    for (f = 0; f < d; f++) {
-        html += `<tr><td>${getNombreEstacion(f)}</td>`;
-        for (c = 0; c < d; c++) {
-            html += `<td>${matriz[f][c]}</td>`;
-        }
-        html+=`<td>${maxFil[f]}</td>`
-        html += `</tr>`;
-    }
-    html += `</table>`;
-    $(ubicacion).html(html);
-}
-function mostrarTablaMatrizA2Max(matriz, ubicacion, d){
-    let html = `
-        <table>
-            <tr>
-                <th></th>`;
-    for (c = 0; c < d; c++) {
-        html += `<th>${getNombreEstacion(c)}</th>`;
-    }
-    html += `</tr>`;
-
-    for (f = 0; f < d; f++) {
-        html += `<tr><td>${getNombreEstacion(f)}</td>`;
-        for (c = 0; c < d; c++) {
-            html += `<td>${matriz[f][c]}</td>`;
-        }
-        html += `</tr>`;
-    }
-    html += `</table>`;
-    $(ubicacion).html(html);
+    console.log(ofertas);
+    console.log(demandas);
 }
